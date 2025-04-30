c265b9a0ee4c8a209e6fa8678b3816419650b2b4
Kumaran172 <kumaran.sujasen@gmail.com>
2025-04-30 15:02:26 +0530

    Update App.jsx
    added background image

--- App.jsx
+++ App.jsx
@@ -2,13 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import backgroundImage from './assets/sc_backgroundImg.jpg'
 
 function App() {
   const [Result, setResult] = useState("");
   const [Number_1, setNumber_1] = useState("");
   const [Number_2, setNumber_2] = useState("");
+
   return (
-    <>
+    <div className="app-container" style={{ backgroundImage: `url(${backgroundImage})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundPosition: 'center', minHeight: '100vh' }}>
       <div className='num1'>
         Number_1 : {""}
         <input
@@ -37,8 +39,8 @@ function App() {
          setResult(parseInt (Number_1) / parseInt(Number_2));
        }}>Div</button>
       </div>
-      <h2>Result is:{Result}</h2>
-    </>
+      <h2>Result is:{Result} <br/>      - MS Kumaran</h2>
+    </div>
   )
 }
 
